453f964649bffcdda82dbb983397e246a92cd425
melalonso <melvin.aep@hotmail.com>
2015-05-18 17:14:54 -0600

    Using angular to display teacher's index page.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,26 @@
-var application = angular.module('RateachApp', []);
+var app = angular.module('Rateach',[]);
+
+var path;
+
+app.factory('teachersInfo', ['$http', function($http) {
+
+    //path = jQuery("#pathForAngular").val();
+    path = window.location+".json";
+
+    return $http.get( path )
+        .success(function(data) {
+            return data;
+        })
+        .error(function(err) {
+            return err;
+        });
+}]);
+
+app.controller('MainController', ['$scope', 'teachersInfo', function($scope, teachInfo) {
+
+    teachInfo.success(function(data) {
+        $scope.teachers = data;
+    });
+
+}]);
+
--- app/assets/javascripts/teachers_info.js
+++
@@ -1,9 +0,0 @@
-application.factory('teachersInfo', ['$http', function($http) {
-    return $http.get('http://localhost:3000/faculties/1/teachers.json')
-        .success(function(data) {
-            return data;
-        })
-        .error(function(err) {
-            return err;
-        });
-}]);
